7b0d0d1dcd3f944c6125829802be1ef515a492cb
keuhdall <lmarques@student.42.fr>
2018-04-21 20:59:48 +0200

    addded registerTower implementation ; started implementing updateConditions implementation

--- src/com/avajLauncher/aircraft/Baloon.java
+++ src/com/avajLauncher/aircraft/Baloon.java
@@ -3,17 +3,18 @@ package com.avajLauncher.aircraft;
 import com.avajLauncher.weather.WeatherTower;
 
 public class Baloon extends Aircraft implements Flyable {
+    private WeatherTower _weatherTower;
     Baloon(String name, Coordinates coordinates) {
         super(name, coordinates);
     }
 
     @Override
     public void updateConditions() {
-
+        _weatherTower.getWeather(_coordinates);//TODO: print output
     }
 
     @Override
     public void registerTower(WeatherTower weatherTower) {
-        
+        _weatherTower = weatherTower;
     }
 }
--- src/com/avajLauncher/aircraft/JetPlane.java
+++ src/com/avajLauncher/aircraft/JetPlane.java
@@ -3,6 +3,7 @@ package com.avajLauncher.aircraft;
 import com.avajLauncher.weather.WeatherTower;
 
 public class JetPlane extends Aircraft implements Flyable {
+    private WeatherTower _weatherTower;
     JetPlane(String name, Coordinates coordinates) {
         super(name, coordinates);
     }
@@ -10,11 +11,11 @@ public class JetPlane extends Aircraft implements Flyable {
 
     @Override
     public void updateConditions() {
-
+        _weatherTower.getWeather(_coordinates);//TODO: print output
     }
 
     @Override
     public void registerTower(WeatherTower weatherTower) {
-
+        _weatherTower = weatherTower;
     }
 }
