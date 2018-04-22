5f7f51f3091e4ca4264cc30256afd9ff21c1c24d
Léonard Marques <lmarques@student.42.fr>
2018-04-23 01:04:58 +0200

    made newAircraft method static

--- src/com/avajLauncher/aircraft/AircraftFactory.java
+++ src/com/avajLauncher/aircraft/AircraftFactory.java
@@ -1,7 +1,7 @@
 package com.avajLauncher.aircraft;
 
 public class AircraftFactory {
-    public Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
+    public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
         Coordinates coordinates = new Coordinates(longitude, latitude, height);
         switch (type) {
             case "Baloon":
