194171c3ef75646c061dc68262218796c07cb4d6
Léonard Marques <lmarques@student.42.fr>
2018-04-23 02:29:17 +0200

    Renamed and moved main ; started main implementation

---
+++ src/com/avajLauncher/weather/Simulator.java
@@ -0,0 +1,56 @@
+package com.avajLauncher.weather;
+
+import com.avajLauncher.aircraft.AircraftFactory;
+import com.avajLauncher.aircraft.Flyable;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Simulator {
+    public static BufferedWriter writer;
+    private static List<Flyable> flyables = new ArrayList<>();
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Please provide a scenario file");
+            return;
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            writer = new BufferedWriter(new FileWriter("simulation.txt"));
+            String line = reader.readLine();
+            if (line == null) return;
+            WeatherTower weatherTower = new WeatherTower();
+            int simulations = Integer.parseInt(line.split(" ")[0]);
+            if (simulations < 0) {
+                System.out.println("Invalid simulations count: " + simulations);
+                System.exit(1);
+            }
+            while ((line = reader.readLine()) != null) {
+                Flyable flyable = AircraftFactory.newAircraft(
+                        line.split(" ")[0], line.split(" ")[1],
+                        Integer.parseInt(line.split(" ")[2]),
+                        Integer.parseInt(line.split(" ")[3]),
+                        Integer.parseInt(line.split(" ")[4]));
+                flyables.add(flyable);
+            }
+            for (Flyable flyable : flyables) {
+                flyable.registerTower(weatherTower);
+            }
+            for (int i = 0; i < simulations; i++) {
+                weatherTower.changeWeather();
+            }
+
+        } catch (IOException | ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
